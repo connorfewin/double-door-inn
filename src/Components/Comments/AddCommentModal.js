a30ecdb3f8805b96692bf7d9cd6dea4a138f0041
Connor Fewin <cjfewin2018@gmail.com>
2025-02-05 12:26:26 -0500

    Added email notification for unverifed comments

--- src/Components/Comments/AddCommentModal.js
+++ src/Components/Comments/AddCommentModal.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Modal, Box, TextField, Button } from '@mui/material';
 import { createCommentAPI } from '../../Api/comment';
 import ImageDropzone from '../ImageDropzone';
+import emailjs from 'emailjs-com';
 
 function AddCommentModal({ open, onClose }) {
   const [formData, setFormData] = useState({
@@ -35,10 +36,36 @@ function AddCommentModal({ open, onClose }) {
         title,
         description,
         author,
+        verified: false,
         images: files.map(file => file.file),
       };
       const createdComment = await createCommentAPI(commentData);
       console.log('Submitted Comment:', createdComment);
+
+      console.log(`Preparing to send email: \n${process.env.REACT_APP_EMAILJS_SERVICE_ID} \n${process.env.REACT_APP_EMAILJS_TEMPLATE_ID}`)
+      // Send email that there is a new unverified comment to cjfewin2018@gmail.com
+      const templateParams = {
+        to_name: author,
+        message: description,
+        num_images: files.length,
+      };
+      emailjs.send(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        templateParams,
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        // Optionally show a success message to the user
+        alert('Your comment was submitted! An admin has been notified.');
+      })
+      .catch((err) => {
+        console.error('FAILED...', err);
+        // Optionally show an error message
+        alert('Oops, something went wrong.');
+      });
+
 
       setFormData({ title: '', description: '', author: '', files: [] });
       onClose();
